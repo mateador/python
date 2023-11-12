67b020d7ee4cbf1d760d4c6016cb14505d909d6b
Alexandre Leite Silva <mateador@users.noreply.github.com>
2023-11-12 10:53:46 +0000

    Gráficos de linhas

--- oi.py
+++ oi.py
@@ -1,7 +1,21 @@
 """
-comentário
+Visualização de dados em Python
 """
-print ("Hello World")
-print (2**3)
 
-print (2*2)
+import matplotlib.pyplot as plt
+
+x = [1, 2, 5]
+y = [2, 3, 7]
+titulo = "Gráfico de barras"
+eixox = "Eixo X"
+eixoy = "Eixo Y"
+
+# Título
+plt.title(titulo)
+
+#Eixos
+plt.xlabel(eixox)
+plt.ylabel(eixoy)
+
+plt.plot(x, y)
+plt.show()
